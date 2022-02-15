6655891f91d210ada0b62226e927fd8578e4430c
Johan Vargas <johanvargasdev@gmail.com>
2022-02-15 11:41:20 -0500

    list propertly filled,

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css'
+import React, { useEffect, useState, useCallback } from 'react'
+
+const concatDogs = async (e) => {
+  const breed = e[0]
+  const childs = e[1]
+  if( childs.length > 0 ) {
+    childs.map(child=>{
+      concatDogs([ `${breed}/${child}`, [] ]).then(r=>r)
+    })
+  }
+  const ElementWithImages = fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(response=>response.json())
+    .then(json => {
+      const newObject = {
+        name: breed.replace('/', ' '),
+        image: json.message
+      }
+      return newObject
+    })
+  return ElementWithImages
+}
+
+function App() {
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    fetch(`https://dog.ceo/api/breeds/list/all`)
+      .then(response=>response.json())
+      .then(json=>{
+        (Object.entries(json.message)).map(e=>{
+          concatDogs(e).then(res=>{
+            setList(list => [...list, res])
+          })
+        })
+      })
+  }, [])
+  return (
+    <div className="wrapper">
+     <h1>My Grocery List</h1>
+     <ul>
+      {Object.entries(list).map(item => <li key={item}>{item}</li>)}
+     </ul>
+   </div>
+  )
+}
+
+export default App
