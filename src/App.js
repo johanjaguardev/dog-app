60db1f7ea64628c7ae2b7d1a22e7ba45075c9b93
Johan Vargas <johanvargasdev@gmail.com>
2022-02-15 16:06:31 -0500

    flat array

--- src/App.js
+++ src/App.js
@@ -1,42 +1,58 @@
 import './App.css'
 import React, { useEffect, useState, useCallback } from 'react'
 
-const concatDogs = async (e) => {
-  const breed = e[0]
-  const childs = e[1]
-  if( childs.length > 0 ) {
-    childs.map(child=>{
-      concatDogs([ `${breed}/${child}`, [] ]).then(r=>r)
-    })
+const getImages = async(breed) => {
+  const images = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+  const imagesJson = await images.json()
+  const imagesMessage = await imagesJson.message
+  const imagesObject = await {
+    name: breed.replace('/', ' '),
+    image: imagesMessage
   }
-  const ElementWithImages = fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
-    .then(response=>response.json())
-    .then(json => {
-      const newObject = {
-        name: breed.replace('/', ' '),
-        image: json.message
-      }
-      return newObject
-    })
-  return ElementWithImages
+  return imagesObject
+}
+const concatDogs = async (dog) => {
+  const breed = dog[0]
+  const childs = dog[1]
+  
+  if( childs.length > 0 ) {
+    const childimages = await Promise.all(childs.map(child=>{
+      const images = getImages(`${breed}/${child}`)
+      return images
+    })) 
+    return childimages
+  } else {
+    const images = getImages(breed)
+    return images
+  }
 }
 
 function App() {
   const [list, setList] = useState([])
-  const dogsArray = []
 
   useEffect(async() => {
     (async () => {
+      const flatDogsArray = []
       const all = await fetch(`https://dog.ceo/api/breeds/list/all`)
       const allJson = await all.json()
       const allMessage = await allJson.message
       const allWithChildrens = await Promise.all(
-        Object.entries(allMessage).map(async (e)=>{
-          return concatDogs(e)
+        Object.entries(allMessage).map(async (dog)=>concatDogs(dog))
+      )
+      const flatDogs = await Promise.all(
+        allWithChildrens.map(async (dog)=>{
+          if( dog.hasOwnProperty('name') ) {
+            flatDogsArray.push(dog)
+          } else {
+            Object.entries(dog).map(child=>{
+              flatDogsArray.push(child[1])
+            })
+          }
+          return flatDogsArray
         })
       )
-      setList(allWithChildrens)
-      console.log(all, allJson, allMessage, allWithChildrens)
+      setList(flatDogsArray)
+      //console.log(all, allJson, allMessage, allWithChildrens, list)
     })();
   }, [])
   return (
