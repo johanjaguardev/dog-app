44d33b4d7db59dfdcfd4008aed51b2ad86faf0eb
Johan Vargas <johanvargasdev@gmail.com>
2022-02-15 18:16:53 -0500

    css changes for mobile view

--- src/App.js
+++ src/App.js
@@ -63,28 +63,30 @@ function App() {
           <h1>Dog's R Us</h1>
           <nav>
             <ul className='header__menu'>
-              <li><a href="#" target="_blank">Home</a></li>
-              <li><a href="#" target="_blank">About</a></li>
-              <li><a href="#" target="_blank">Contact Us</a></li>
+              <li className='header__menu-li'><a className="header__menu-a" href="#" target="_blank">Home</a></li>
+              <li className='header__menu-li'><a className="header__menu-a" href="#" target="_blank">About</a></li>
+              <li className='header__menu-li'><a className="header__menu-a" href="#" target="_blank">Contact Us</a></li>
             </ul>
           </nav>
         </header>
         <div className='hero'>
           <h2>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,</h2>
           <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-          <div className="hero__video-boc">
-            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/VqUeo5SRMx4" allowfullscreen></iframe>
+          <div className="hero__video-box">
+            <iframe className="hero__videos" width="100%" height="100%" src="https://www.youtube.com/embed/VqUeo5SRMx4" allowfullscreen></iframe>
           </div>
         </div>
       </div>
 
-      <div className="dog__container container">
+      <div className="dogs dogs__container container">
         <h3>Dog Breeds</h3>
-        <ul className="dog__grid">
+        <ul className="dogs__grid">
           {Object.entries(list).map(item =>
-          <li key={item[0]} className="dog__item">
-            <img src={item[1].image}/>
-            <h4>{item[1].name}</h4>
+          <li key={item[0]} className="dogs__item">
+            <figure className='dogs__figure'>
+              <img src={item[1].image} className="dogs__img"/>
+              <figcaption className='dogs__figcaption'>{item[1].name}</figcaption>
+            </figure>
           {item[1].video}
           </li>)}
         </ul> 
